25b4b37202fccf2edc864762c09c2f3c900ac34c
belkotzsti <belkot1p2025@gmail.com>
2026-03-12 09:45:50 +0100

    Update and rename zadanie3 to zadanie3.php

--- czesc6/zadanie3.php
+++ czesc6/zadanie3.php
@@ -1,4 +1,6 @@
+<?php
 $string3 = "poniedziałek.wtorek.środa";
 $incorrectlyexplodedstring = explode(",", $string3);
 print_r($incorrectlyexplodedstring);
 // cały string przypisze się do jednego indeksu w tablicy
+?>
