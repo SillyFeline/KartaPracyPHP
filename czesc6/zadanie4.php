87992e6f16d45cfeff01c766b193c22533445b26
belkotzsti <belkot1p2025@gmail.com>
2026-03-12 09:46:51 +0100

    Update zadanie4.php

--- czesc6/zadanie4.php
+++ czesc6/zadanie4.php
@@ -1,6 +1,6 @@
 <?php
 $string4 = "poniedziałek,wtorek,środa,czwartek";
-$limitedexplodedstring = explode(",", $string4, 3);
+$limitedexplodedstring = explode(",", $string4, 3); // ograniczamy dodając maksymalną liczbę części na koniec
 print_r($limitedexplodedstring);
 // dwie części tekstu przypiszą się do jednego indeksu
 ?>
