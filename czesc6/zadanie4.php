0c814b5ec082edf8c527726a2a78ae1d5f4bafc2
belkotzsti <belkot1p2025@gmail.com>
2026-03-12 09:46:07 +0100

    Update and rename zadanie4 to zadanie4.php

--- czesc6/zadanie4.php
+++ czesc6/zadanie4.php
@@ -1,4 +1,6 @@
+<?php
 $string4 = "poniedziałek,wtorek,środa,czwartek";
 $limitedexplodedstring = explode(",", $string4, 3);
 print_r($limitedexplodedstring);
 // dwie części tekstu przypiszą się do jednego indeksu
+?>
